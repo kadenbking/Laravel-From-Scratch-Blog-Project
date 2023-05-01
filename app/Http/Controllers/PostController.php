51294a24d709614199b7ae0ff3ab82084c43b16f
Kaden King <70766937+kadenbking@users.noreply.github.com>
2023-05-01 13:15:34 -0500

    lower pagination to 6

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
         return view('posts.index', [
             'posts' => Post::latest()->filter(
                         request(['search', 'category', 'author'])
-                    )->paginate(18)->withQueryString()
+                    )->paginate(6)->withQueryString()
         ]);
     }
 
